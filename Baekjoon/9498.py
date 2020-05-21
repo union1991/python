7eed01918336b5d815a2adc15cf4255c3d7b4b2c
union1991 <56064985+union1991@users.noreply.github.com>
2020-05-21 15:56:26 +0900

    Create 9498.py

---
+++ Baekjoon/9498.py
@@ -0,0 +1,10 @@
+# 시험 성적 점수 매기기
+
+a = input()
+a = int(a)
+
+if(100>=a>=90) : print("A")
+elif(89>=a>=80) : print("B")
+elif(79>=a>=70) : print("C")
+elif(69>=a>=60) : print("D")
+else : print("F")
