d9ac43ac3d06e404b4504ca2748a9b9e0edabf78
union1991 <56064985+union1991@users.noreply.github.com>
2020-05-26 16:07:53 +0900

    Update 15552.py

--- Baekjoon/15552.py
+++ Baekjoon/15552.py
@@ -1,1 +1,18 @@
+# input 대신 sys.stdin.readline을 사용하여 다수의 합을 구하여라
 
+'''
+n = int(input())
+
+for i in range(0, n) :
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    print(a+b)
+'''
+import sys
+
+n = int(input())
+
+for i in range(n) :
+    a, b = map(int, sys.stdin.readline().split())
+    print(a+b)
